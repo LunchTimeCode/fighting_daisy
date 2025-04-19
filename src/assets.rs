afa02c62a3bdcd4fbf20a677c64232b3f256ab1d
SilenLoc <silen.locatelli@optravis.com>
2025-04-19 17:41:06 +0200

    asset as option

--- src/assets.rs
+++ src/assets.rs
@@ -3,10 +3,7 @@ use std::path::Path;
 
 static PROJECT_DIR: Dir<'_> = include_directory!("assets");
 
-pub fn read_any_file(name: &str) -> File {
+pub fn read_any_file(name: &str) -> Option<&File<'_>> {
     let path = Path::new(name);
-    let file = PROJECT_DIR
-        .get_file(path)
-        .unwrap_or_else(|| panic!("could not find file this name: {}", path.to_str().unwrap()));
-    file.clone()
+    PROJECT_DIR.get_file(path)
 }
